4260d833d17e45a92a3989de850b55625a0ef65e
DmytroLysachenko <dlysachenko98@gmail.com>
2025-05-19 19:15:32 +0200

    Removed completely workflows from project as overcomplication, any changes via Admin console will be carried out on local host for economy of server traffic.

--- db/schema/serverState.ts
+++
@@ -1,7 +0,0 @@
-import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
-
-export const serverState = pgTable("server_state", {
-  id: text("id").primaryKey(),
-  status: boolean("status").default(false),
-  updatedAt: timestamp("updated_at").defaultNow(),
-});
